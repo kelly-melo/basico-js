4775ec6bce8a4befe78c80e7eddcc63417210bf2
kelly-melo <eustassrock@gmail.com>
2024-03-10 18:30:51 -0300

    meses do ano

---
+++ meses.js
@@ -0,0 +1,44 @@
+let n1 = prompt("Insira um número de 1 a 12")
+
+switch (n1) {
+
+    case "1":
+        alert("Este número se refere ao mês de janeiro")
+        break;
+        case "2":
+        alert("Este número se refere ao mês de fevereiro")
+        break;
+        case "3":
+        alert("Este número se refere ao mês de março")
+        break;
+        case "4":
+        alert("Este número se refere ao mês de abril")
+        break;
+        case "5":
+        alert("Este número se refere ao mês de maio")
+        break;
+        case "6":
+        alert("Este número se refere ao mês de junho")
+        break;
+        case "7":
+        alert("Este número se refere ao mês de julho")
+        break;
+        case "8":
+        alert("Este número se refere ao mês de agosto")
+        break;
+        case "9":
+        alert("Este número se refere ao mês de setembro")
+        break;
+        case "10":
+        alert("Este número se refere ao mês de outubro")
+        break;
+        case "11":
+        alert("Este número se refere ao mês de novembro")
+        break;
+        case "12":
+        alert("Este número se refere ao mês de dezembro")
+        break;
+    default:
+        alert("O número que você inseriu não corresponde a nenhum mês")
+        break;
+}
