61e90ad2dfd28213c73b1a2dbba8bbd16f030531
kelly-melo <eustassrock@gmail.com>
2024-03-14 23:58:00 -0300

    média de arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+let numeros = [11, 65, 6, 4, 87]
+let variavelSoma = 0
+for (i = 0; i < numeros.length; i++) {
+variavelSoma += numeros[i];
+}
+let media = alert(`A média dos números de sua array é igual a ${variavelSoma/i}`)
